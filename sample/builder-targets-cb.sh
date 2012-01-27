e1664ea0e7663036ef55e5ad212569d062dce48f
DT Netsolution GmbH <daniel.schwager@dtnet.de>
2012-01-27 11:08:27 +0100

    add more docu

--- sample/builder-targets-cb.sh
+++ sample/builder-targets-cb.sh
@@ -1,15 +1,18 @@
 ##############################################################################
 # This optional file "builder-targets-cb.sh" will be called by builder.sh 
 # 
-# You can overwrite target functions like
-#   config, prepare, retrieve, create, package, publish, commit, cleanup
+# The targets will be called from thde opsi-builder using the following
+# order: config, prepare, retrieve, create, package, publish, commit, cleanup 
+# You can overwrite the target functions in builder-targets-cb.sh
 # 
-# You can define callback functions like
+# You can define callback functions. The functions are called from
+# opsi-builder within processing a target
 #   cb_package_makeproductfile
 #
 # You can use every variable defined in any configuration file or by
 # the defined builder script itself. Also, calling the predefined
 # targets builder_<targetname> is possible.
+#
 #
 ##############################################################################
 
